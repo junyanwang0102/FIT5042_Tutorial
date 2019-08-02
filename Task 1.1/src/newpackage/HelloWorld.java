a7da5379edce6106ce610af62a788390cc44e6e8
junyanwang0102 <48872674+junyanwang0102@users.noreply.github.com>
2019-08-02 13:53:38 +1000

    Task 1.1 Helloworld Task

---
+++ Task 1.1/src/newpackage/HelloWorld.java
@@ -0,0 +1,14 @@
+package newpackage;
+
+import java.util.List;
+
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+
+
+    }
+}
