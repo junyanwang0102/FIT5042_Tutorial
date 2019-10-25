68a4d59db17db9f00023382fdea1a0f5122ec728
junyanwang0102 <48872674+junyanwang0102@users.noreply.github.com>
2019-10-25 23:32:59 +1100

    Add files via upload

---
+++ Task 10-1/src/main/java/org/fit5042/pokemon/repository/PokemonRepository.java
@@ -0,0 +1,8 @@
+package org.fit5042.pokemon.repository;
+import org.fit5042.pokemon.entity.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PokemonRepository extends JpaRepository<Pokemon, Long>
+{
+
+}
